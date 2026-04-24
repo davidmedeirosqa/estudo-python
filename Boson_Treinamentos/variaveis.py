4fc1420261868738edc056c647d998de804f0adf
David Medeiros <davidmedeiros94@hotmail.com>
2026-04-24 18:38:08 -0300

    Finalizado estudo sobre variável

--- Boson_Treinamentos/variaveis.py
+++ Boson_Treinamentos/variaveis.py
@@ -1,1 +1,21 @@
-nome = "David"
+# nome = "David"
+# print(type(nome))
+
+# media = 0
+# print(type(media))
+
+# estado = True
+# print(type(estado))
+
+# print(type(1+2j)) #Complex
+
+# # Função isinstance()
+# a = 10
+# b = "Sol"
+# print(isinstance(a, int)) # Verifica se a variável a é do tipo int retornando True ou False
+# print(isinstance(b, bool)) # Verifica se a variável b é do tipo bool retornando True ou False
+
+a = 40
+c = 3
+r = a*c
+print(r)
