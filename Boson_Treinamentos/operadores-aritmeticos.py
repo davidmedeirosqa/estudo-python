8464b647f033213721291eeff47e3b501b897683
David Medeiros <davidmedeiros94@hotmail.com>
2026-04-24 21:11:25 -0300

    treinamento com op. aritméticos e de comparação

---
+++ Boson_Treinamentos/operadores-aritmeticos.py
@@ -0,0 +1,7 @@
+x = y = z = 0
+
+x = int(input("Digite um número: ")) # Casting - Conversão de string para int
+y = int(input("Digite outro número: ")) # Casting - Conversão de string para int
+
+z = x + y
+print(f"A soma dos dois valores é {z}") # f - Formatação de strings
