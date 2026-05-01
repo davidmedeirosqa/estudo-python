d973d92694a2966af95f88a6bc51ea95af357ff0
David Medeiros <davidmedeiros94@hotmail.com>
2026-05-01 08:41:11 -0300

    Criado a lógica

---
+++ Hashtag/meu_arquivo.py
@@ -0,0 +1,12 @@
+# Importar a base de dados
+import pandas as pd
+
+# Visualizar a base de dados
+
+# Faturamento por loja
+
+# Quantidade de produtos vendidos por loja
+
+# Ticket médio por produto em cada loja
+
+# Enviar um e-mail com o relatório
